d0464d435c1c1430f6261340f3a2f639ad7dd21f
orcavasanth <vasanthcuddalore@gmail.com>
2014-08-20 16:54:50 -0430

    second edited

---
+++ testlin2.C
@@ -0,0 +1,12 @@
+/* chapter 3 test line program
+* Vasanth, Aug 2014 */
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Test Programming lines\n");
+  printf("Test\n ..1\n....2\n......3\n");
+
+  return 0;
+}
