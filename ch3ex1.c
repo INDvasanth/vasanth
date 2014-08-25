9a632eb8fd4413bad0ac0a73300f8be33144dd84
orcavasanth <vasanthcuddalore@gmail.com>
2014-08-25 16:01:21 -0430

    Exercies

---
+++ ch3ex1.c
@@ -0,0 +1,21 @@
+/* Chapter 3 Exercises program
+* Vasanth 25 Aug 2014 */
+
+#include <stdio.h>
+ 
+int main (void)
+{
+  
+ printf ("Testing...");
+ printf ("....1");
+ printf ("...2");
+ printf ("..3");
+ printf ("\n");
+
+ return 0;
+
+}
+
+Output:
+ Testing.......1...2..3
+ 
---
+++ ch3ex2.c
@@ -0,0 +1,24 @@
+/* Chapter 3 Exercise - subtracts value 15 from 87
+* Vasanth 25 Aug 2014 */
+
+#include <stdio.h>
+
+int main (void)
+{
+ 
+ int value1;
+ int value2;
+ int sum;
+
+ value1 = 15;
+ value2 = 87;
+ sum = value2 - value1;
+ 
+ printf ("The subtraction of %i from %i is %i", value1, value2, sum);
+ 
+ return 0;
+
+}
+
+Output:
+ The subtraction of 15 from 87 is 72
