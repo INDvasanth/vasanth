96c6d4ea44bbfd90f34da97a1f84cca11c343ff7
orcavasanth <vasanthcuddalore@gmail.com>
2014-08-25 16:28:11 -0430

    Ch3Exercies

---
+++ ch3ex3.c
@@ -0,0 +1,22 @@
+/* Chapter 3 Exercises syntactic errors corretion
+* Vasanth 25 Aug 2014 */
+
+#include <stdio.h>
+
+int main (void)
+{
+
+ int sum;
+
+ /* compute Result */
+ sum = 25 + 37 - 19;
+ 
+ /* Display Result */
+ printf ("The Answer is %i\n", sum);
+
+ return 0;
+
+}
+
+Output:
+ The answer is 43
---
+++ ch3ex4.c
@@ -0,0 +1,21 @@
+/* Chapter 3 Exercises calculation
+* Vasanth 25 Aug 2014 */
+
+#include <stdio.h>
+
+int main(void)
+{
+ 
+ int answer;
+ int result;
+ 
+ answer = 100;
+ result = answer - 10;
+ printf ("The result is %i\n", result+5);
+
+ return 0;
+
+}
+
+Output:
+ The result is 95
