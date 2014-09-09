ffa9a5d5cec67665b19513533ed569b6ff9d669c
orcavasanth <vasanthcuddalore@gmail.com>
2014-09-09 18:55:25 +0530

    ex

--- ch3ex3.c
+++ ch3ex3.c
@@ -1,22 +1,23 @@
-/* Chapter 3 Exercises syntactic errors corretion
+/*Chapter 3 Exercises Syntactic errors corretion
 * Vasanth 25 Aug 2014 */
 
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
+	int sum;
 
- int sum;
+	/* coputer result */
+	sum = 25 + 37 - 19;
+	
+	/* Display result */
+	printf("The Answer is %i\n", sum);
 
- /* compute Result */
- sum = 25 + 37 - 19;
- 
- /* Display Result */
- printf ("The Answer is %i\n", sum);
-
- return 0;
+	return 0;
 
 }
 
+/*
 Output:
- The answer is 43
+	The answer is 43
+*/
