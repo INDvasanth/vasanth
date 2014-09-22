38a4a47a5bc3a4d4a2717426a66725dba6fe6a40
orcavasanth <vasanthcuddalore@gmail.com>
2014-09-22 14:49:06 +0530

    ch10

---
+++ Chapter10/Ch10Example1.c
@@ -0,0 +1,40 @@
+/* Chapter-10 Example-10.1 Program to concatenate two character arrays
+* Vasanth [21 September 2014] */
+
+#include <stdio.h>
+
+void concat(char result[], const char str1[], int n1, const char str2[], int n2)
+{
+	int i;
+	int j;
+	
+	/* Copy str1 to result */
+	
+	for (i = 0; i < n1; ++i)
+		result [i] = str1[i];
+		
+	/* Copy str2 to result */
+	
+	for (j = 0; j < n2; ++j)
+		result[n1 + j] = str2[j];
+			
+}
+
+int main(void)
+{
+	void concat(char result[], const char str1[], int n1, const char str2[], int n2);
+	
+	const char s1[5] = {'T', 'e', 's', 't'};
+	const char s2[6] = {'W', 'o', 'r', 'k'};
+	char s3[11];
+	int i;
+	
+	concat(s3, s1, 5, s2, 6);
+	
+	for (i = 0; i < 11; ++i)
+		printf("%c", s3[i]);
+		
+	printf("\n");
+	
+	return 0;
+}
